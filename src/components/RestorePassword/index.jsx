e748eb5506b9ced8038d57fedadc7565a9ee431d
lns <bezborodnikovtimur@gmail.com>
2025-05-07 23:34:34 +0300

    password-reset request added

--- src/components/RestorePassword/index.jsx
+++ src/components/RestorePassword/index.jsx
@@ -1,16 +1,30 @@
 import React, {useState} from 'react';
 import s from "./styles.module.css"
 import {useNavigate} from 'react-router-dom';
+import axios from "axios";
+import {apiAuthURL} from "../../configs/constants.js";
 
 const RestorePassword = () => {
     const navigate = useNavigate();
     const [email, setEmail] = useState("")
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
+
         if (email.trim().length) {
-            alert(`Письмо для сброса пароля отправлено на почту: ${email}`)
-            navigate("/login");
+            try {
+                await axios.post(`${apiAuthURL}/password-reset/request`, { email })
+
+                alert(`Письмо для сброса пароля отправлено на почту: ${email}`)
+                navigate("/login")
+            } catch (error) {
+                if (error.response) {
+                    alert(`Ошибка: ${error.response.data.message || "Не удалось отправить письмо."}`)
+                } else {
+                    alert("Ошибка сети. Попробуйте ещё раз.")
+                }
+                console.error("Ошибка запроса:", error)
+            }
         }
     }
     
